5aa62c3654e0f243fe574407c613b7137aabe873
PavloVerbytsky <pavelemtyfy@gmail.com>
2022-08-07 12:21:33 +0300

    / hello

---
+++ src/parts/TopBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const TopBar = () => {
+  return (
+    <div className="topbar">
+      <div className="logo">
+        <img src="" alt="" />
+      </div>
+      <div className="buttons">
+        <a href="#" className="button">Home</a>
+        <a href="#" className="button">Products</a>
+        <a href="#" className="button">Contacts</a>
+        <a href="#" className="button">About Us</a>
+      </div>
+    </div>
+  );
+};
